498888044659c6b618d2f8f34d00fd2ebb924829
DESKTOP-LJHP9LK\rafif <mrpwt0302@gmail.com>
2026-02-15 15:01:26 +0700

    fix deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo "Starting Spring Boot on port 8080 with memory optimization..."
-# Tambahkan -Xss256k untuk menghemat stack memory per thread
-exec java -Xmx160m -Xms160m -Xss256k -XX:+UseSerialGC \
+echo "Starting Spring Boot..."
+# Kita kurangi Xms agar startup tidak langsung memakan banyak RAM
+exec java -Xmx170m -Xms64m -Xss256k -XX:+UseSerialGC \
+     -Dspring.main.lazy-initialization=true \
      -Dserver.address=0.0.0.0 \
      -Dserver.port=8080 \
-     -Dspring.main.lazy-initialization=true \
      -jar /app/app.jar
